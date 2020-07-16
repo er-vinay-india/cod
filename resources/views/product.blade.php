4fd937c5f1578bbe2e15f2179835373980148a50
divyanshu <divyanshu.shivhare32@gmail.com>
2020-07-16 10:56:33 +0530

    changes further made

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel TRY</title>
+        <title>Laravel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,17 +81,22 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                     DIVYANSHU
+                    Laravel
+                        <ul>
+                    <?php foreach($products as $product) { ?>
+                        <li><a href="http://127.0.0.1:8000/users"><?php echo $product->title; ?></a></li>
+                    <?php } ?>
+                        </ul>
                 </div>
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">COVID LIVE</a>
+                    <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">TWITTER</a>
-                    <a href="https://forge.laravel.com">FACEBOOK</a>
-                    <a href="https://vapor.laravel.com">INSTAGRAM</a>
+                    <a href="https://nova.laravel.com">Nova</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://vapor.laravel.com">Vapor</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
